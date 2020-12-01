a9ff550e80e2467522180e7c4948a0ddd2444727
TheDexire <thedexire@bk.ru>
2020-12-01 13:36:02 +0600

    - minor export fix

--- app/Exports/ReportingExport.php
+++ app/Exports/ReportingExport.php
@@ -6,10 +6,13 @@ use App\Models\Course;
 use Maatwebsite\Excel\Concerns\FromArray;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithStrictNullComparison;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class ReportingExport implements FromArray, WithHeadings, ShouldAutoSize, WithStrictNullComparison
+class ReportingExport implements FromArray, WithHeadings, ShouldAutoSize, WithColumnWidths, WithStyles, WithStrictNullComparison
 {
     /**
      * @return \Illuminate\Support\Collection
@@ -19,6 +22,21 @@ class ReportingExport implements FromArray, WithHeadings, ShouldAutoSize, WithSt
     public function __construct(array $invoices)
     {
         $this->invoices = $invoices;
+    }
+
+    public function columnWidths(): array
+    {
+        return [
+            'B' => 50,
+            'C' => 50,
+        ];
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        $sheet->getStyle('B2:B999')->getAlignment()->setWrapText(true);
+        $sheet->getStyle('C2:C999')->getAlignment()->setWrapText(true);
+        $sheet->getStyle('A1:K1')->getFont()->setBold(true);
     }
 
 
