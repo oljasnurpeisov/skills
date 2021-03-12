4a7b5271ec0a6be08587a8e33f39a6ac01ecd314
jokerkz <jokerkaz@gmail.com>
2021-03-12 14:10:05 +0600

    - member role fix

--- app/Models/Dialog.php
+++ app/Models/Dialog.php
@@ -48,17 +48,23 @@ class Dialog extends Model
         }
 
         $member = $members->first();
-        $memberRole = $member->roles()->first();
-
-        if ($memberRole != null) {
-            if ($memberRole->slug == 'author') {
-                $name = $member->author_info->name;
-                $avatar = $member->author_info->getAvatar();
-                $slug = '';
-            } else if ($memberRole->slug == 'student') {
-                $name = $member->student_info->name ?? __('default.pages.profile.student_title');
-                $avatar = $member->student_info->getAvatar();
-                $slug = '';
+        $roles = $member->roles();
+        if ($roles != null) {
+            $memberRole = $roles->first();
+            if ($memberRole != null) {
+                if ($memberRole->slug == 'author') {
+                    $name = $member->author_info->name;
+                    $avatar = $member->author_info->getAvatar();
+                    $slug = '';
+                } else if ($memberRole->slug == 'student') {
+                    $name = $member->student_info->name ?? __('default.pages.profile.student_title');
+                    $avatar = $member->student_info->getAvatar();
+                    $slug = '';
+                } else {
+                    $name = $member->name;
+                    $avatar = '';
+                    $slug = 'tech_support';
+                }
             } else {
                 $name = $member->name;
                 $avatar = '';
