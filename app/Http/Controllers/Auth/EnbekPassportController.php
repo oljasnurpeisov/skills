ac4ac946f9eca550bc7fca5b986e450051efdeb7
Kirill Gurovoy <kgurovoy@MBP-Kirill.lan>
2021-06-07 12:43:59 +0300

    Enbek Passport

--- app/Http/Controllers/Auth/EnbekPassportController.php
+++ app/Http/Controllers/Auth/EnbekPassportController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Dialog;
+use App\Models\PayInformation;
 use App\Models\User;
+use App\Models\UserInformation;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Libraries\Auth\EnbekPassport;
@@ -48,6 +51,8 @@ class EnbekPassportController extends Controller
             $this->loginByEmail();
         }
 
+        $this->register((object) ['email' => 'test@testtest.test3']);
+
         if (Auth::check()) {
             return redirect(url((new LoginController())->redirectTo()));
         }
@@ -64,13 +69,38 @@ class EnbekPassportController extends Controller
 
         $user = $this->user->whereEmail($passportUser->email)->first();
 
-        dump(1);
-        dump($user);
 
         if (!empty($user)) {
             Auth::login($user, true);
         } else {
-            dd("user not found");
+            $this->register($passportUser);
         }
+    }
+
+    protected function register($passportUser)
+    {
+        $user = $this->user->create(['email' => $passportUser->email]);
+
+        $user_information = new UserInformation;
+        $user_information->user_id = $user->id;
+        $user_information->save();
+
+        $user_pay_information = new PayInformation;
+        $user_pay_information->user_id = $user->id;
+        $user_pay_information->save();
+
+        $user->roles()->sync([4]);
+
+        // Создание диалога с тех.поддержкой
+        $tech_support = User::whereHas('roles', function ($q) {
+            $q->where('slug', '=', 'tech_support');
+        })->first();
+
+        $dialog = new Dialog;
+        $dialog->save();
+
+        $dialog->members()->sync([$user->id, $tech_support->id]);
+
+        Auth::login($user, true);
     }
 }
