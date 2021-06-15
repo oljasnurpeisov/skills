189c7a8c392b1bd0a0183d392434a2e4996ad9c9
Kirill Gurovoy <kgurovoy@MBP-Kirill.lan>
2021-06-15 10:21:37 +0300

    Fix

--- app/Libraries/Auth/AuthStudent.php
+++ app/Libraries/Auth/AuthStudent.php
@@ -104,12 +104,12 @@ class AuthStudent
                 $studentInformation->save();
             } else {
                 Session::put('resume_data', $user->id);
-                return redirect('/')->send();
+                return redirect(url('/'))->send();
             }
         }
 
         if ($user->roles()->first()->id != $studentRole->id) {
-            return redirect('/')->with('status', __('default.pages.auth.student_login_author_exist'))->send();
+            return redirect(url('/')->with('status', __('default.pages.auth.student_login_author_exist'))->send();
         }
 
         Session::put('student_token', $this->token);
