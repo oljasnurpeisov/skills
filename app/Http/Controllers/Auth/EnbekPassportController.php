1a0a3e8804693b4cd8884028cacd8b063c6ea34f
Kirill Gurovoy <kgurovoy@MBP-Kirill.lan>
2021-06-15 11:58:46 +0300

    Fix

--- app/Http/Controllers/Auth/EnbekPassportController.php
+++ app/Http/Controllers/Auth/EnbekPassportController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Libraries\Auth\AuthEnbekPassport;
 use Service\Auth\AuthService;
+use Services\Auth\LoginService;
 
 class EnbekPassportController extends Controller
 {
@@ -17,17 +18,23 @@ class EnbekPassportController extends Controller
      * @var AuthEnbekPassport
      */
     private $authEnbekPassport;
+    /**
+     * @var LoginService
+     */
+    private $loginService;
 
     /**
      * EnbekPassportController constructor.
      *
      * @param AuthService $authService
      * @param AuthEnbekPassport $authEnbekPassport
+     * @param LoginService $loginService
      */
-    public function __construct(AuthService $authService, AuthEnbekPassport $authEnbekPassport)
+    public function __construct(AuthService $authService, AuthEnbekPassport $authEnbekPassport, LoginService $loginService)
     {
         $this->authService          = $authService;
         $this->authEnbekPassport    = $authEnbekPassport;
+        $this->loginService         = $loginService;
     }
 
     /**
@@ -36,5 +43,11 @@ class EnbekPassportController extends Controller
     public function login()
     {
         $this->authEnbekPassport->init();
+
+        if (Auth::check()) {
+            return redirect(url($this->loginService->redirect()));
+        } else {
+            return redirect(url('/'));
+        }
     }
 }
