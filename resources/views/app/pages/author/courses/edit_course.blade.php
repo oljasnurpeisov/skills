87016a70bec0c390db5e773e514045e1161f84f5
olzhas.nurpeisov <olzhas.nurpeisov@inessoft.kz>
2021-06-30 15:16:39 +0600

    edit course template

--- resources/views/app/pages/author/courses/edit_course.blade.php
+++ resources/views/app/pages/author/courses/edit_course.blade.php
@@ -612,16 +612,72 @@
                             </div>
                             <div class="form-group">
                                 <label class="form-group__label">{{__('default.pages.courses.preview_certificate_title')}}</label>
+                                <input name="certificate_id" type="hidden" value="{{$item->certificate_id}}">
                                 <div class="row row--multiline">
+{{--                                    <div class="col-auto">--}}
+{{--                                        <div class="image-choice">--}}
+{{--                                            <img src="/assets/img/certificates/4.png"--}}
+{{--                                                 class="image-choice__thumbnail"--}}
+{{--                                                 alt="">--}}
+{{--                                            <label class="image-choice__overflow">--}}
+{{--                                                <i> </i>--}}
+{{--                                                <a href="/assets/img/certificates/4.png" data-fancybox title="{{__('default.pages.courses.zoom_certificate')}}"--}}
+{{--                                                   class="icon-zoom-in" style="margin-top: 20px"> </a>--}}
+{{--                                            </label>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
                                     <div class="col-auto">
-                                        <div class="image-choice">
-                                            <img src="/assets/img/certificates/4.png"
+                                        <div class="image-choice {{ $item->certificate_id ==  '11' ? 'checked' : ''  }} " data-id="11">
+                                            <img src="/assets/img/certificates/cert_new_11.jpg"
                                                  class="image-choice__thumbnail"
                                                  alt="">
                                             <label class="image-choice__overflow">
                                                 <i> </i>
-                                                <a href="/assets/img/certificates/4.png" data-fancybox title="{{__('default.pages.courses.zoom_certificate')}}"
-                                                   class="icon-zoom-in" style="margin-top: 20px"> </a>
+                                                {{--                                                <a class="check" style="margin-top: -22px;">✓</a>--}}
+                                                <a href="/assets/img/certificates/cert_new_11.jpg" data-fancybox title="{{__('default.pages.courses.zoom_certificate')}}"
+                                                   class="icon-zoom-in" style="margin-top: 23px"> </a>
+                                            </label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-auto">
+                                        <div class="image-choice {{ $item->certificate_id ==  '12' ? 'checked' : ''  }} " data-id="12">
+                                            <img src="/assets/img/certificates/cert_new_12.jpg"
+                                                 class="image-choice__thumbnail"
+                                                 alt="">
+                                            <label class="image-choice__overflow">
+                                                <i> </i>
+                                                {{--                                                <a class="check" style="margin-top: -22px;">✓</a>--}}
+                                                <a href="/assets/img/certificates/cert_new_12.jpg" data-fancybox title="{{__('default.pages.courses.zoom_certificate')}}"
+                                                   class="icon-zoom-in" style="margin-top: 23px"> </a>
+                                            </label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-auto">
+                                        <div class="image-choice {{ $item->certificate_id ==  '13' ? 'checked' : ''  }} " data-id="13">
+                                            <img src="/assets/img/certificates/cert_new_13.jpg"
+                                                 class="image-choice__thumbnail"
+                                                 alt="">
+                                            <label class="image-choice__overflow">
+                                                <i> </i>
+                                                {{--                                                <a class="check" style="margin-top: -22px;">✓</a>--}}
+                                                <a href="/assets/img/certificates/cert_new_13.jpg" data-fancybox title="{{__('default.pages.courses.zoom_certificate')}}"
+                                                   class="icon-zoom-in" style="margin-top: 23px"> </a>
+                                            </label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-auto">
+                                        <div class="image-choice {{ $item->certificate_id ==  '10' ? 'checked' : ''  }} " data-id="10">
+                                            <img src="/assets/img/certificates/cert_new_10.jpg"
+                                                 class="image-choice__thumbnail"
+                                                 alt="">
+                                            <label class="image-choice__overflow">
+                                                <i> </i>
+                                                {{--                                                <a class="check" style="margin-top: -22px;">✓</a>--}}
+                                                <a href="/assets/img/certificates/cert_new_10.jpg" data-fancybox title="{{__('default.pages.courses.zoom_certificate')}}"
+                                                   class="icon-zoom-in" style="margin-top: 23px"> </a>
                                             </label>
                                         </div>
                                     </div>
@@ -641,10 +697,35 @@
         </section>
 
     </main>
+
+    <style>
+        .image-choice.checked::after {
+            content: "";
+            position: absolute;
+            top: -10px;
+            right: -11px;
+            height: 25px;
+            width: 25px;
+            background: url(/assets/img/checked.png);
+            background-repeat: no-repeat;
+            background-image: cover;
+        }
+        .image-choice {
+            width: 9rem;
+        }
+    </style>
 @endsection
 
 @section('scripts')
     <!--Only this page's scripts-->
+    <script>
+        $('.image-choice').on('click', function () {
+            $('.image-choice').removeClass('checked');
+            let block = $(this);
+            block.addClass('checked');
+            $('input[name="certificate_id"]').val(block.attr('data-id'));
+        });
+    </script>
     <script>
         const professionalAreaEl = $('[name="professional_areas[]"]'),
             specialityEl = $('[name="professions[]"]'),
