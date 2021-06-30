0b5124575a2da2dfdfe8527c1d300965acdde2e4
Kirill Gurovoy <kgurovoy@MBP-Kirill.lan>
2021-06-30 10:55:57 +0300

    Fix translate

--- resources/views/app/layout/default/components/modals.blade.php
+++ resources/views/app/layout/default/components/modals.blade.php
@@ -286,7 +286,7 @@
         </form>
     </div>
     <div id="agreeModal" style="display:none;" class="modal-form">
-        <h2 class="title-primary">{{__('default.pages.private_policy.private_policy_title')}}</h2>
+        <h2 class="title-primary" style="text-align: center">{{__('default.pages.private_policy.private_policy_title')}}</h2>
         <form action="/{{$lang}}/agree/{{session('agree_data')}}" method="post">
             @csrf
             <div class="form-group">
