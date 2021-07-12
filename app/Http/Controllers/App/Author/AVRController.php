93f322ecb4886183e32a799ff69bb6beb87fba9d
Eugene Kozlov <eudjin@gmail.com>
2021-07-12 14:25:37 +0600

    Request language

--- app/Http/Controllers/App/Author/AVRController.php
+++ app/Http/Controllers/App/Author/AVRController.php
@@ -116,7 +116,7 @@ class AVRController extends Controller
      * @param UpdateAVR $request
      * @return RedirectResponse
      */
-    public function update(UpdateAVR $request, Request $allRequest): RedirectResponse
+    public function update(UpdateAVR $request): RedirectResponse
     {
         /** @var AVR $act */
         $act = AVR::where('id', $request->avr_id)->firstOrFail();
@@ -127,7 +127,7 @@ class AVRController extends Controller
 
         if ($act->invoice_link && $act->number && $act->author_signed_at) {
             $this->authorAVRService->acceptAvr($act->id);
-            return redirect()->route('author.avr.index', ['lang' => $allRequest->lang]);
+            return redirect()->route('author.avr.index', ['lang' => 'ru']);
         }
 
         return redirect()->back();
