819a1bcb256deb7702920d6beffd935fd3b16f53
olzhas.nurpeisov <olzhas.nurpeisov@inessoft.kz>
2021-11-22 14:49:11 +0600

    authors page

--- app/Http/Controllers/App/General/AuthorsController.php
+++ app/Http/Controllers/App/General/AuthorsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\App\General;
 
 use App\Http\Controllers\Controller;
 use App\Models\Course;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -11,17 +12,42 @@ class AuthorsController extends Controller
 {
     public function authorsCatalog(Request $request, $lang)
     {
-        $query = Course::where('status', '=', Course::published);
+        $term = $request->search ? $request->search : '';
+        $query = User::whereHas('roles', function ($q) {
+            $q->whereSlug('author');
+        });
+        if ($term) {
+            $query = $query->where('company_name', 'like', '%' . $term . '%');
+        }
+        $items = $query->paginate(8);
+        foreach ($items as $item) {
+            $courses = $item->courses()->get();
+            // Все оценки всех курсов
+            $rates = [];
+            foreach ($courses as $course) {
+                foreach ($course->rate as $rate) {
+                    array_push($rates, $rate->rate);
+                }
+            }
+            // Все ученики автора
+            $author_students = [];
+            foreach ($courses as $course) {
+                foreach ($course->course_members as $member) {
+                    $author_students[$member['student_id']][] = $member;
+                }
+            }
+            // Оценка автора исходя из всех оценок
+            if (count($rates) == 0) {
+                $average_rates = 0;
+            } else {
+                $average_rates = array_sum($rates) / count($rates);
+            }
 
-//        $topSkills = DB::table('student_skills')
-//            ->select('skill_id', DB::raw('count(*) as total'))
-//            ->groupBy('skill_id')
-//            ->orderBy('total', 'desc')
-//            ->take(30)
-//            ->join('skills', 'student_skills.skill_id', '=', 'skills.id')
-//            ->addSelect("skills.name_{$lang} as name")
-//            ->get();
-        $items = $query->paginate(6);
+            $item->rates = $rates;
+            $item->courses = $courses;
+            $item->author_students = $author_students;
+            $item->average_rates = $average_rates;
+        }
         return view("app.pages.general.authors.authors_catalog", [
             "items" => $items,
             'request' => $request,
