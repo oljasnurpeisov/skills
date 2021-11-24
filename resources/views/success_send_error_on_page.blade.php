9f8a905a1fffbb25940c4d1a170b42bb883daf6a
DK <dara.kamaliev@gmail.com>
2021-11-24 16:30:57 +0600

    #profile change link

--- resources/views/success_send_error_on_page.blade.php
+++ resources/views/success_send_error_on_page.blade.php
@@ -4,7 +4,7 @@
     <main class="main">
         <div class="container">
             <div class="row">
-                <div class="col-md-12 text-center">
+                <div class="col-md-12 text-center" style="padding-bottom: 90px">
                     <h1>{{ __('default.thanks_for_error') }}</h1>
                     <a class="btn" href="{{ url()->previous() }}">{{ __('default.back') }}</a>
                 </div>
